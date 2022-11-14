f9ff504e80bcda5078d7b04ff59a82804fe6d975
s0575347 <johannes.plath@student.htw-berlin.de>
2022-11-14 16:58:12 +0100

    clear sth

--- main.py
+++ main.py
@@ -52,16 +52,16 @@ if __name__ == '__main__':
     # plt.show()
     print_dim_min_max_and_return_max_min(one_ch_im)
     img = cv2.imread('CT.png')
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
-
-    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnt = contours[0]
-    x,y,w,h = cv2.boundingRect(cnt)
-    print('x,y,w,h:', x,y,w,h)
-    crop = img[y:y+h,x:x+w]
-    cv2.imwrite('CT_new.png',crop)
-     cropped_img = crop(one_ch_im)
+    # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+    #
+    # contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    # cnt = contours[0]
+    # x,y,w,h = cv2.boundingRect(cnt)
+    # print('x,y,w,h:', x,y,w,h)
+    # crop = img[y:y+h,x:x+w]
+    # cv2.imwrite('CT_new.png',crop)
+    cropped_img = crop(one_ch_im)
     print(  'cropped_img.shape: ', cropped_img.shape)
     cv2.imwrite('CT_new.png',cropped_img)
     plt.close()
