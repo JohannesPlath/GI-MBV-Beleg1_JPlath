6844da873222546e096bb707041b28eb2b51d13a
s0575347 <johannes.plath@student.htw-berlin.de>
2022-12-13 15:44:28 +0100

    find median cut fin

--- median_cut.py
+++ median_cut.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 from numpy import median, mean
+import matplotlib.pyplot as plt
 
 img_path = 'CT.png'
 img_to_save = 'to_save.png'
 img_to_save2 = 'to_save_2.png'
 
 image = cv2.cvtColor (cv2.imread (img_path), cv2.COLOR_BGR2RGB)
-
 
 
 def find_biggest_range_cluster_list(cluster_list):
@@ -32,91 +32,88 @@ def find_biggest_range_cluster_list(cluster_list):
 
 
 def find_median_cut_in_array_with_num_of_cluster(array_in_2_d, num_cluster):
-    while array_in_2_d.__len__() < num_cluster:
+    while array_in_2_d.__len__ () < num_cluster:
         cluster_value_dimension = find_biggest_range_cluster_list (array_in_2_d)
-        cluster_to_split = array_in_2_d.pop(cluster_value_dimension.get("cluster"))
+        cluster_to_split = array_in_2_d.pop (cluster_value_dimension.get ("cluster"))
         median_list = []
         for item in cluster_to_split:
-            median_list.append(item[cluster_value_dimension.get("right/dimension")])
-        t = median(median_list)
-        cluster_smaller = cluster_to_split[np.where(cluster_to_split[:, cluster_value_dimension.get("right/dimension")] <= t)]
-        cluster_larger = cluster_to_split[np.where(cluster_to_split[:, cluster_value_dimension.get("right/dimension")] > t)]
-        array_in_2_d.append(cluster_larger)
-        array_in_2_d.append(cluster_smaller)
+            median_list.append (item[cluster_value_dimension.get ("right/dimension")])
+        t = median (median_list)
+        cluster_smaller = cluster_to_split[
+            np.where (cluster_to_split[:, cluster_value_dimension.get ("right/dimension")] <= t)]
+        cluster_larger = cluster_to_split[
+            np.where (cluster_to_split[:, cluster_value_dimension.get ("right/dimension")] > t)]
+        array_in_2_d.append (cluster_larger)
+        array_in_2_d.append (cluster_smaller)
     resultlist = []
     for cluster in array_in_2_d:
         mittelwerte = []
-        for richtung in range(cluster[0].shape[0]):
-            mittelwerte.append(int(round(np.mean(cluster[:, richtung]))))
-        resultlist.append(mittelwerte)
-        res =  np.array(resultlist).reshape(resultlist.__len__(), 3)
-    res = sorted(res, key=lambda x:x.sum())
+        for richtung in range (cluster[0].shape[0]):
+            mittelwerte.append (int (round (np.mean(cluster[:, richtung]))))
+        resultlist.append (mittelwerte)
+        res = np.array (resultlist).reshape (resultlist.__len__ (), 3)
+    res = sorted (res, key=lambda x: x.sum ())
     return res
 
 
-def find_new_color(item, clusterlist):
-    x = 0;
-    while x < clusterlist.__len__() -1 :
-        first = int(round(clusterlist[x].sum()))
-        second =int(round(clusterlist[x+1].sum()))
-        diff_one = item.sum() - first
-        diff_two = second - item.sum()
-        if (diff_one < diff_two ):
-            return clusterlist[x]
-        x = x +1
-    return clusterlist[0]
+def find_new_color(item, color_list):
+    x = 0 ;
+    while x <= color_list.__len__ () - 1:
+        first = int (round (color_list[x].sum ()))
+        second = int (round (color_list[x + 1].sum ()))
+        diff_one = item.sum () - first
+        diff_two = second - item.sum ()
+        if (diff_one < diff_two):
+            return color_list[x]
+        if (x == color_list.__len__ () - 2):
+            return color_list[x + 1]
+        x = x + 1
+    return color_list[0]
 
 
 
+def median_cut(img_path, path_to_save, color_count):
+    img = cv2.imread (img_path)
+    imgList = [np.array (img).reshape (img.shape[0] * img.shape[1], img.shape[2])]
+    mediancut_list = find_median_cut_in_array_with_num_of_cluster (imgList, color_count)
+    # correct color range
+    result = np.eye (img.shape[0] * img.shape[1], img.shape[2])
+    next_img = np.array (img).reshape (img.shape[0] * img.shape[1], img.shape[2])
+    for first_dim, arr in enumerate (next_img):
+        result[first_dim] = find_new_color (arr, mediancut_list)
+    # generate Integer
+    int_image = result.astype(int)
 
-
+    # correct failures
+    int_image[np.where (int_image == np.max (int_image))] = 0
+    int_image[np.where (int_image == np.min (int_image))] = 0
+    print ("resultImage.shape bofor .reshape()", int_image.shape)
+    int_image2 = int_image.reshape (img.shape[0], img.shape[1], img.shape[2])
+    print ("resultImage.shape ", int_image2.shape)
+    print ("mediancut_list", mediancut_list)
+    print ("result_image ", int_image2)
+    # save image
+    image = int_image2.astype(np.uint8)
+    cv2.imshow ('matrix', image)
+    cv2.waitKey (0)
+    cv2.imwrite (path_to_save, image)
+    return image
 
 if __name__ == '__main__':
 
+    # cluster_list2 = np.array(img).reshape(-1, img.shape[2])
+    # img = np.array(tmp_img)
 
-    img = cv2.imread('Baboon.png')
-    #cluster_list2 = np.array(img).reshape(-1, img.shape[2])
-    #img = np.array(tmp_img)
-    imgList = [np.array(img).reshape(img.shape[0] * img.shape[1], img.shape[2])]
-    print ("<imgList ", imgList)
+    # print ("<imgList ", imgList)
     # print("cluster_list.pop().shape ", cluster_list.pop().shape)
     # print("imgList.pop().shape ", imgList.pop().shape)
+    # mediancut_list2 = mediancut_list[:][:]
+    # imgList = mediancut_list
 
 
-
-    mediancut_list = find_median_cut_in_array_with_num_of_cluster(imgList, 16)
-
-
-    #mediancut_list2 = mediancut_list[:][:]
-
-
-    #imgList = mediancut_list
-
-    result = np.eye(img.shape[0] * img.shape[1], img.shape[2])
-    next_img = np.array(img).reshape(img.shape[0] * img.shape[1], img.shape[2])
-    for first_dim, arr in enumerate(next_img):
-           result[first_dim] = find_new_color(arr, mediancut_list )
-
-    result2 = np.eye(img.shape[0] * img.shape[1], img.shape[2])
-    for first, arr2 in enumerate(result):
-        for second, arr1 in enumerate(arr2):
-                result2[first][second]= int(round(arr1))
-
-    resultImage = np.array(result2)
-    resultImage[np.where(resultImage==np.max(resultImage))] = 0
-    resultImage[np.where(resultImage==np.min(resultImage))] = 0
-    print("resultImage.shape ",resultImage.shape )
-    resultImage = resultImage.reshape(img.shape[0] , img.shape[1], img.shape[2])
-    print("resultImage.shape ", resultImage.shape)
-    print ("mediancut_list", mediancut_list)
-    print("result ", result)
-
-    cv2.imshow('matrix', resultImage)
-    cv2.waitKey(0)
-    cv2.imwrite(img_to_save2, resultImage)
-
-
-
-    #print ("mediancut_list.pop().pop().dtype", mediancut_list.pop().pop().dtype)
-
-
+    #median_cut('Baboon.png', img_to_save2, 2)
+    # print ("mediancut_list.pop().pop().dtype", mediancut_list.pop().pop().dtype)
+    result_image = median_cut('Baboon.png', 'Baboon_2_color.png', 16)
+    plt.imshow(result_image)
+    plt.show()
+    plt.close()
